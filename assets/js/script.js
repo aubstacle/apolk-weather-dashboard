ac34ea07ac20a8e262dbb726bc88c9c067b310de
Aubrey Polk <aubstacle3@gmail.com>
2020-09-16 14:07:12 -0400

    successfully called on current city, temp, windspeed, humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,29 @@ var searchBtn = $("#searchButton");
 var clearBtn = $("#clearButton");
 var searchHistory = $("#searchHistory");
 var fiveForecast = $("#fiveForecast");
+var cityNameEl = $(".cityName");
+var tempEl = $(".temperature");
+var humidityEl = $(".humidity");
+var windEl = $(".wind");
+var uvIndexEl = $(".uv");
+
+// On click for searching cities //
+searchBtn.on("click", function (event) {
+  event.preventDefault();
+  var city = cityInput.val();
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=739e8f5c71af00f6952140bc0cc3d4e2";
+  $.ajax({
+    method: "GET",
+    url: queryURL,
+  }).then(function (response) {
+    console.log(queryURL);
+    console.log(response);
+    console.log(response.name);
+    cityNameEl.text(response.name);
+    tempEl.text("Temperature: " + response.main.temp + "˚F");
+    humidityEl.text("Humidity: " + response.main.humidity);
+  });
+});
