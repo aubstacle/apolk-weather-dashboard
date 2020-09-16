c7ca65b37f578150ed9f5b73df484b09194bb23f
Aubrey Polk <aubstacle3@gmail.com>
2020-09-15 21:51:01 -0400

    added text navbar, current time and date

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// These elements and function use moment.js to display the current date and time //
+var dayEl = $("#currentDay");
+var timeEl = $("#currentTime");
+var currentDate = moment().format("dddd MMMM Do");
+currentTime = moment().format("LTS");
+
+dayEl.text(currentDate);
+timeEl.text(currentTime);
+
+setInterval(function () {
+  var clock = moment().format("h:mm:ss A");
+  timeEl.text(clock);
+}, 1000);
